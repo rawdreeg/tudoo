96972ed67c05643257f10df4d98ec0dc433c51ec
Rodrigue Tusse <rawdreeg@gmail.com>
2015-12-25 19:40:24 +0200

    Tudoo app by rawdreeg

--- tudoo/tudoo.js
+++ tudoo/tudoo.js
@@ -1,23 +1,84 @@
+Todos = new Mongo.Collection('todos');
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+    Meteor.subscribe('todos');
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+    //Template Helpers
+    Template.main.helpers({
+        todos: function(){
+            return Todos.find({}, {sort: {createdAt: -1}});
+        }
+    });
+
+    Template.main.events({
+       "submit .new-todo": function(event){
+           var text = event.target.text.value;
+           Meteor.call('addTodo', text)
+           //clear form
+
+           event.target.text.value ='';
+
+           //prevents submit
+           return false;
+       },
+        "click .toggle-checked": function(){
+            Meteor.call('setChecked', this._id, !this.checked)
+        },
+        "click .delete-todo": function(){
+            if(confirm("Are you sure")){
+                Meteor.call('deleteTodo', this._id)
+            }
+
+        }
+    });
+
+    Accounts.ui.config({
+        passwordSignupFields: "USERNAME_ONLY"
+    });
+
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.publish('todos', function(){
+        if(!this.userId){
+
+            return "";
+
+        } else {
+            return Todos.find({userId: this.userId});
+        }
+    });
 }
+
+
+//Methods
+Meteor.methods({
+    addTodo: function(text){
+        if(!Meteor.userId){
+            throw new Meteor.Error("Not Authorized");
+        }
+        Todos.insert({
+            text: text,
+            createdAt: new Date(),
+            userId: Meteor.userId(),
+            username: Meteor.user().username
+        });
+    },
+    deleteTodo: function(todoId){
+        var todo = Todos.findOne(todoId);
+        if(todo.userId !== Meteor.userId()){
+            throw new Meteor.Error("Not Authorized");
+        }
+        Todos.remove(todoId);
+    },
+    setChecked: function(todoId, setChecked){
+        var todo = Todos.findOne(todoId);
+        if(todo.userId !== Meteor.userId()){
+            throw new Meteor.Error("Not Authorized");
+        }
+        Todos.update(todoId, {$set:{checked: setChecked}});
+    }
+
+});
+
